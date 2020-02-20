769c8dacede1f1ac4ea3ccde8b6bc9e198b2d9bf
gupta_keshav_001378461 <gupta.k@husky.neu.edu>
2020-02-20 14:48:39 -0500

    4th Commit

---
+++ Assignment4/src/Business/Ticket/Ticket.java
@@ -0,0 +1,105 @@
+
+package Business.Ticket;
+
+import Business.Airliner.Airliner;
+import Business.Airliner.Flight.*;
+import Business.TravelOffice.Customer.Customer;
+import Business.TravelOffice.TravelOffice;
+
+
+public class Ticket {
+    // interact with the flight.seat / flight.seatAssignment
+    // interact with customer
+
+    
+    private static int count =0;
+    
+    private Flight flight;
+    private Customer customer;
+    
+    // mark which office book this ticket
+    private String ticketNumber;
+    private String bookOffice;
+    private String seatInfo;
+    private boolean pending;
+    private String flightNumber;
+    
+    public Ticket(Flight flight, Customer customer, String seatInfo, String officeCode){
+
+//      flight.SeatAssignment(customer, seatNumber);       
+        this.ticketNumber = officeCode + Integer.toString(count ++);
+        this.bookOffice = officeCode;
+        this.seatInfo = seatInfo;
+        this.flight = flight;
+        this.customer = customer;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Ticket.count = count;
+    }
+
+    public String getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(String ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public String getBookOffice() {
+        return bookOffice;
+    }
+
+    public void setBookOffice(String bookOffice) {
+        this.bookOffice = bookOffice;
+    }
+
+    public String getFlightInfo() {
+        return flight.getFlightNo();
+    }
+
+    public String getSeatInfo() {
+        return seatInfo;
+    }
+
+    public String getCustomerInfo() {
+        return "Name: " + customer.getName() + "ID: " + customer.getIdentityID();
+    }
+
+    public String getAirplaneInfo() {
+        return flight.getAirplane().toString();
+    }
+
+    public boolean isPending() {
+        return pending;
+    }
+
+    public void setPending(boolean pending) {
+        this.pending = pending;
+    }
+
+    public String getFlightNumber() {
+        return flight.getFlightNo();
+    }
+    
+    public Flight getFlight(){
+        return flight;
+    }
+
+    @Override
+    public String toString(){
+        return ticketNumber;
+    }
+    public String getAirlinerName(){
+        return flight.getAirliner().getName();
+    }
+    
+    public Airliner getAirliner(){
+        return flight.getAirliner();
+    }
+    
+}
---
+++ Assignment4/src/Business/UserAccount/UserAccount.java
@@ -0,0 +1,82 @@
+
+package Business.UserAccount;
+
+import Business.Airliner.Airliner;
+import Business.TravelOffice.TravelOffice;
+
+/**
+ *
+ * @author keshav
+ */
+public class UserAccount {
+     
+    private String username;
+    private String password;
+    private String role;
+    
+    private Airliner airliner;
+    private TravelOffice travelOffice;
+
+    public UserAccount(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        
+    }
+    
+    public UserAccount(){
+        
+    }
+    
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public void setMember(Airliner airliner){
+        this.airliner = airliner;
+        this.travelOffice = null;
+    }
+    
+    public void setMember(TravelOffice travelOffice){
+        this.travelOffice = travelOffice;
+        this.airliner = null;
+    }
+    
+    public Object getMember(){
+        if(this.travelOffice != null){
+            return this.travelOffice;
+        }else if(this.airliner != null){
+            return this.airliner;
+        }
+        return null;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    
+    @Override
+    public String toString() {
+        return username;
+    }
+    
+    
+}
